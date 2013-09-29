d7dd5ebddd450cdadc13160d96f4895297e57a53
Derek Deike <derek.deike@gmail.com>
2013-09-29 10:54:11 -0700

    php updated to comment out test var

--- hw1/logic.php
+++ hw1/logic.php
@@ -3,8 +3,8 @@
 		date_default_timezone_set('America/Los_Angeles');
 
 		//$timeofday is being calcuated by date()
-		//$timeofday = date('G');
-		$timeofday = 21;
+		$timeofday = date('G');
+		//$timeofday = 21;
 
 		// here's the logic. shifting styling to css. keeping php here.
 		if ($timeofday < 6 OR $timeofday > 20) {
